6d0eb5666a1fc4823201f6f0729782f3e0683cdd
Damien Autrusseau <autrusseau.damien@gmail.com>
2018-08-28 13:47:23 +0200

    small fixes about service image

---
+++ docker_emperor/commands/docker/reset.py
@@ -0,0 +1,12 @@
+import docker_emperor.logger as logger
+
+'''
+or docker-machine ssh virtualbox -- tce-load -wi rsync
+'''
+def run(root, *args, **kwargs):
+
+    root.bash(
+        'docker rm $(docker ps -a -q) -f',
+        compose=root.compose,
+        is_system=True
+    )
